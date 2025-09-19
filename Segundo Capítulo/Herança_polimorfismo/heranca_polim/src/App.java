91fb9d0ea9b6e25eb6dd5d2250984eed800346e1
Davi vaccarezza <76525758+Davivaca@users.noreply.github.com>
2025-09-19 17:44:35 -0300

    First Projects

---
+++ Segundo Capítulo/Herança_polimorfismo/heranca_polim/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Manager manager = new Manager();
+
+        manager.setName("João");
+        manager.setLogin("joão");
+        manager.setPassword("123456");
+
+        
+    }
+}
